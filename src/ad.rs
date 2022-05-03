57643f14bae9d6ffdd8898222dd3aa2726bfd91b
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-05-03 10:38:34 +0200

    more operators

--- src/ad.rs
+++ src/ad.rs
@@ -20,16 +20,19 @@ struct Tape {
 
 type GradientsMap = HashMap<usize, Vec<f32>>;
 
+pub trait NumberLike:
+    ops::Add +
+    ops::AddAssign +
+    ops::Sub
+    where
+        Self: Sized
+    {}
+
+
 pub struct AD {
     tapes: RefCell<HashMap<usize, Tape>>,
     max_tape_id: RefCell<usize>,
     gradients: RefCell<GradientsMap>,
-}
-
-impl std::fmt::Debug for AD {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        writeln!(f, "AD {{ tapes: {:#?} }}", self.tapes.borrow())
-    }
 }
 
 #[derive(Copy, Clone)]
@@ -85,6 +88,155 @@ impl <'a> ADNumber<'a> {
 
     pub fn diff(&self, wrt: &ADNumber) -> f32 {
         self.ad.diff(self, wrt)
+    }
+
+    pub fn powi(&self, operand: i32) -> Self {
+        self.ad.create_unary_composite(
+            self, self.scalar.powi(operand),
+            self.scalar.powi(operand - 1) * operand as f32,
+        )
+    }
+
+    pub fn powf(&self, operand: f32) -> Self {
+        self.ad.create_unary_composite(
+            self, self.scalar.powf(operand),
+            self.scalar.powf(operand - 1.0) * operand,
+        )
+    }
+
+    pub fn exp(&self) -> Self {
+        let exp = self.scalar.exp();
+        self.ad.create_unary_composite(
+            self, exp, exp,
+        )
+    }
+
+    pub fn sin(&self) -> Self {
+        let sin = self.scalar.sin();
+        self.ad.create_unary_composite(
+            self, sin, self.scalar.cos(),
+        )
+    }
+
+    pub fn cos(&self) -> Self {
+        let cos = self.scalar.cos();
+        self.ad.create_unary_composite(
+            self, cos, -self.scalar.sin(),
+        )
+    }
+
+    pub fn tan(&self) -> Self {
+        let tan = self.scalar.tan();
+        self.ad.create_unary_composite(
+            self, tan, 1.0 + tan * tan,
+        )
+    }
+
+    pub fn asin(&self) -> Self {
+        let asin = self.scalar.asin();
+        self.ad.create_unary_composite(
+            self, asin, 1.0 / (1.0 - self.scalar * self.scalar).sqrt(),
+        )
+    }
+
+    pub fn acos(&self) -> Self {
+        let acos = self.scalar.acos();
+        self.ad.create_unary_composite(
+            self, acos, -1.0 / (1.0 - self.scalar * self.scalar).sqrt(),
+        )
+    }
+
+    pub fn atan(&self) -> Self {
+        let atan = self.scalar.atan();
+        self.ad.create_unary_composite(
+            self, atan, 1.0 / (1.0 + self.scalar * self.scalar),
+        )
+    }
+
+    pub fn sinh(&self) -> Self {
+        let sinh = self.scalar.sinh();
+        self.ad.create_unary_composite(
+            self, sinh, self.scalar.cosh(),
+        )
+    }
+
+    pub fn cosh(&self) -> Self {
+        let cosh = self.scalar.cosh();
+        self.ad.create_unary_composite(
+            self, cosh, self.scalar.sinh(),
+        )
+    }
+
+    pub fn tanh(&self) -> Self {
+        let tanh = self.scalar.tanh();
+        self.ad.create_unary_composite(
+            self, tanh, 1.0 - tanh * tanh,
+        )
+    }
+
+    pub fn asinh(&self) -> Self {
+        let asinh = self.scalar.asinh();
+        self.ad.create_unary_composite(
+            self, asinh, 1.0 / (self.scalar + self.scalar * self.scalar).sqrt(),
+        )
+    }
+
+    pub fn acosh(&self) -> Self {
+        let acosh = self.scalar.acosh();
+        self.ad.create_unary_composite(
+            self, acosh, 1.0 / (self.scalar * self.scalar - 1.0).sqrt(),
+        )
+    }
+
+    pub fn atanh(&self) -> Self {
+        let atanh = self.scalar.atanh();
+        self.ad.create_unary_composite(
+            self, atanh, 1.0 / (1.0 - self.scalar * self.scalar),
+        )
+    }
+
+    pub fn sqrt(&self) -> Self {
+        let sqrt = self.scalar.sqrt();
+        self.ad.create_unary_composite(
+            self, sqrt, 0.5 / sqrt,
+        )
+    }
+
+    pub fn cbrt(&self) -> Self {
+        let cbrt = self.scalar.cbrt();
+        self.ad.create_unary_composite(
+            self, cbrt, 1.0 / 3.0 * (self.scalar / cbrt).powi(2),
+        )
+    }
+
+    pub fn abs(&self) -> Self {
+        let abs = self.scalar.abs();
+        self.ad.create_unary_composite(
+            self, abs, if self.scalar < 0.0 { -1.0 } else { 1.0 },
+        )
+    }
+
+    pub fn relu(&self) -> Self {
+        let relu = self.scalar.max(0.0);
+        self.ad.create_unary_composite(
+            self, relu, if self.scalar > 0.0 { 1.0 } else { 0.0 },
+        )
+    }
+
+    pub fn leaky_relu(&self, alpha: f32) -> Self {
+        let leaky_relu = self.scalar.max(0.0).max(self.scalar * alpha);
+        self.ad.create_unary_composite(
+            self, leaky_relu, if self.scalar > 0.0 { 1.0 } else { alpha },
+        )
+    }
+}
+
+impl <'a> NumberLike for ADNumber<'a> {
+}
+
+impl std::fmt::Debug for AD {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f, "AD {{ tapes: {:#?} }}", self.tapes.borrow())
     }
 }
 
@@ -302,6 +454,58 @@ impl <'a> ops::SubAssign<ADNumber<'a>> for ADNumber<'a> {
     }
 }
 
+impl <'a> ops::Mul<ADNumber<'a>> for ADNumber<'a> {
+    type Output = ADNumber<'a>;
+
+    fn mul(self, other: ADNumber<'a>) -> ADNumber {
+        self.ad.create_binary_composite(
+            &self, &other,
+            self.scalar * other.scalar,
+            other.scalar, self.scalar,
+        )
+    }
+}
+
+impl <'a> ops::Mul<f32> for ADNumber<'a> {
+    type Output = ADNumber<'a>;
+
+    fn mul(self, other: f32) -> ADNumber<'a> {
+        self.ad.create_unary_composite(&self, self.scalar * other, other)
+    }
+}
+
+impl <'a> ops::MulAssign<ADNumber<'a>> for ADNumber<'a> {
+    fn mul_assign(&mut self, other: ADNumber<'a>) {
+        *self = *self * other;
+    }
+}
+
+impl <'a> ops::Div<ADNumber<'a>> for ADNumber<'a> {
+    type Output = ADNumber<'a>;
+
+    fn div(self, other: ADNumber<'a>) -> ADNumber {
+        self.ad.create_binary_composite(
+            &self, &other,
+            self.scalar / other.scalar,
+            1.0 / other.scalar, -self.scalar / other.scalar.powi(2),
+        )
+    }
+}
+
+impl <'a> ops::Div<f32> for ADNumber<'a> {
+    type Output = ADNumber<'a>;
+
+    fn div(self, other: f32) -> ADNumber<'a> {
+        self.ad.create_unary_composite(&self, self.scalar / other, 1.0 / other)
+    }
+}
+
+impl <'a> ops::DivAssign<ADNumber<'a>> for ADNumber<'a> {
+    fn div_assign(&mut self, other: ADNumber<'a>) {
+        *self = *self / other;
+    }
+}
+
 mod tests {
     use super::*;
 
@@ -316,5 +520,18 @@ mod tests {
         assert_eq!((y + ad.create_constant(7.0)).diff(&x), 2.0);
         assert_eq!((y + 7.0).diff(&x), 2.0);
         assert_eq!((y + 7.0).scalar, 11.0);
+    }
+
+    #[test]
+    fn test_mul_sub() {
+        let ad = AD::new();
+        let x = ad.create_variable(2.0);
+        let y = x - x;
+        let z = ad.create_variable(3.0);
+        let t = x * z - y;
+        println!("{:#?}", ad);
+        assert_eq!(t.diff(&x), 3.0);
+        assert_eq!(t.diff(&z), 2.0);
+        assert_eq!(t.diff(&y), -1.0);
     }
 }
