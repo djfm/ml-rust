43e338609cb90f751d325ec19cfb90ff7a85d7da
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-05-03 18:16:37 +0200

    some polishing

--- src/ml/mod.rs
+++ src/ml/mod.rs
@@ -3,15 +3,20 @@ mod ad;
 mod scalar_number;
 mod math;
 
-pub use ad::{
-    AD,
+pub use crate::ml::{
+    math::{
+        NumberLike,
+    },
+    ad::{
+        AD,
+    }
 };
 
 mod tests {
+    use super::*;
 
     #[test]
     fn test_add() {
-        use super::{AD};
         let ad = AD::new();
         let x = ad.create_variable(2.0);
         let y = x + x;
@@ -25,7 +30,6 @@ mod tests {
 
     #[test]
     fn test_mul_sub() {
-        use super::{AD};
         let ad = AD::new();
         let x = ad.create_variable(2.0);
         let y = x - x;
@@ -35,5 +39,36 @@ mod tests {
         assert_eq!(t.diff(&x), 3.0);
         assert_eq!(t.diff(&z), 2.0);
         assert_eq!(t.diff(&y), -1.0);
+    }
+
+    #[test]
+    fn test_dx2_dx() {
+        let ad = AD::new();
+        let x = ad.create_variable(2.0);
+        let y = x * x;
+        let dy_dx = y.diff(&x);
+        assert_eq!(dy_dx, 4.0);
+    }
+
+    #[test]
+    fn test_dx2y_dx_dx2y_dy() {
+        let ad = AD::new();
+        let x = ad.create_variable(2.0);
+        let y = ad.create_variable(3.0);
+        let o = x * x * y;
+
+        assert_eq!(o.diff(&x), 12.0);
+        assert_eq!(o.diff(&y), 4.0);
+    }
+
+    #[test]
+    fn test_much_more_complex_diff() {
+        let ad = AD::new();
+        let x = ad.create_variable(3.0);
+        let y = ad.create_variable(4.0);
+        let o = y / (x.exp() - y);
+
+        assert_eq!(o.diff(&x), -0.310507656);
+        assert_eq!(o.diff(&y), 0.077626914);
     }
 }
